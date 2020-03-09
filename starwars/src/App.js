a13c3917639ccd8f4aaa5e69573b4c168bcf236b
Matthew Castillo <blacksheep3y@gmail.com>
2020-03-08 20:51:36 -0400

    officially and successfully was able to log the data as an array and map through it

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 const App = () => {
@@ -8,13 +10,26 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [character, setCharacter] = useState([])
 
+  useEffect(() => {
+
+    axios
+      .get('https://swapi.co/api/people')
+      .then(res => setCharacter(res.data.results))
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log('CHARACTER: ', character)
+  
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      {/* <h1 className="Header">React Wars</h1> */}
+      {character.map(swCharacter => (
+        console.log('SWCHAR: ', {swCharacter})
+      ))}
     </div>
   );
 }
 
 export default App;
-//Matthew Castillo
